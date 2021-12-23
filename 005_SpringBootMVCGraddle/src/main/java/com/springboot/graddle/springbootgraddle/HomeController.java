d4273e8f46265ca40f83d540319f1a9db6cd9460
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-23 08:19:46 -0800

    Adding log capability in spring boot

--- 005_SpringBootMVCGraddle/src/main/java/com/springboot/graddle/springbootgraddle/HomeController.java
+++ 005_SpringBootMVCGraddle/src/main/java/com/springboot/graddle/springbootgraddle/HomeController.java
@@ -1,14 +1,20 @@
 package com.springboot.graddle.springbootgraddle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
     @GetMapping("/hello")
     public String SayHello(@RequestParam(name="username", defaultValue = "gradle venkat")String name)
     {
+        logger.info("my custom log "+name);
+        logger.error("my custom log "+name);
         return "Hello "+name+" !";
     }
 }
