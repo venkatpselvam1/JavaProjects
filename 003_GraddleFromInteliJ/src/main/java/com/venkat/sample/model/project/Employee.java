825c3a913af9a587f2a10d5f066e5eb35fdcc4c4
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-23 00:56:30 -0800

    adding to string method in employee class and using it in the main project

--- 003_GraddleFromInteliJ/src/main/java/com/venkat/sample/model/project/Employee.java
+++ 003_GraddleFromInteliJ/src/main/java/com/venkat/sample/model/project/Employee.java
@@ -3,4 +3,12 @@ package com.venkat.sample.model.project;
 public class Employee {
     public String name;
     public Integer age;
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
--- 003_GraddleFromInteliJ/src/main/java/com/venkat/sample/project/Main.java
+++ 003_GraddleFromInteliJ/src/main/java/com/venkat/sample/project/Main.java
@@ -1,8 +1,14 @@
 package com.venkat.sample.project;
 
+import com.venkat.sample.model.project.Employee;
+
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Hello world");
+        var emp = new Employee();
+        emp.name = "venkat";
+        emp.age  = 12;
+        System.out.println(emp);
     }
 }
