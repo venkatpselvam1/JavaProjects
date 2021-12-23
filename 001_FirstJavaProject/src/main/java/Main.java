3a8bf93b62ead4b7f31782129aff008099946330
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-22 22:40:16 -0800

    Modified the code to print pascals triangle and debugged

--- 001_FirstJavaProject/src/main/java/Main.java
+++ 001_FirstJavaProject/src/main/java/Main.java
@@ -3,5 +3,32 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         System.out.println("Hello world");
+        PrintPascalTriangle(5);
+    }
+    public static void PrintPascalTriangle(int n)
+    {
+        var lis = new int[1];
+        lis[0] = 1;
+        Print(lis);
+        for(int i = 2; i <= n; i++)
+        {
+            var newLis = new int[i];
+            newLis[0] = 1;
+            for(int j = 1; j < i-1;j++)
+            {
+                newLis[j] = lis[j-1]+lis[j];
+            }
+            newLis[i-1] = 1;
+            lis = newLis;
+            Print(lis);
+        }
+    }
+    public static void Print(int[] arr)
+    {
+        for(var item: arr)
+        {
+            System.out.print(item);
+        }
+        System.out.println();
     }
 }
