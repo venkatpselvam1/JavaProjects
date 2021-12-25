a4ff3eb3a1351ff061338dc4e7e42e2e28aca477
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-25 05:19:49 -0800

    Addoing main method and run.xml file

--- 007_SpringFrameworkGradle/src/main/java/app/Main.java
+++ 007_SpringFrameworkGradle/src/main/java/app/Main.java
@@ -1,2 +1,8 @@
-package app;public class Main {
+package app;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("It is gradle spring context app");
+    }
 }
