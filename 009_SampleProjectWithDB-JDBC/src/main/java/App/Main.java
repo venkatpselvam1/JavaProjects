05559858940ea366f1844cc131c9d781f7b30ea1
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-12-25 06:38:23 -0800

    Using JDBC and connecting to the MS SQL to fetch the data

--- 009_SampleProjectWithDB-JDBC/src/main/java/App/Main.java
+++ 009_SampleProjectWithDB-JDBC/src/main/java/App/Main.java
@@ -5,13 +5,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import App.service.Dependency;
 
+import java.sql.DriverManager;
+
 public class Main {
 
-    public static void main(String[] arr)
+    public static void main(String[] arr) throws Exception
     {
+        // To register MS SQL driver
+        DriverManager.registerDriver(new com.microsoft.sqlserver.jdbc.SQLServerDriver());
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        var dependency = applicationContext.getBean("dependency", Dependency.class);
-        dependency.Print("venkat", 5);
         System.out.println("maven spring context project");
         StudentsRepository studentRepository = applicationContext.getBean("studentRepository", StudentsRepository.class);
         var students = studentRepository.GetAllStudents();
--- 009_SampleProjectWithDB-JDBC/src/main/java/App/repository/StudentsRepositoryDataBaseImpl.java
+++ 009_SampleProjectWithDB-JDBC/src/main/java/App/repository/StudentsRepositoryDataBaseImpl.java
@@ -4,6 +4,7 @@ import App.model.Student;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,7 +15,32 @@ public class StudentsRepositoryDataBaseImpl implements StudentsRepository {
     public List<Student> GetAllStudents()
     {
         var lis = new ArrayList<Student>();
-        // need to implement. need to connect to db.
+        Connection conn = null;
+
+        try {
+
+            String dbURL = "jdbc:sqlserver://localhost;databaseName=school;username=diner;password=diner;";
+            conn = DriverManager.getConnection(dbURL);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM student");
+            while(rs.next()){
+                var student = new Student();
+                student.setAge(rs.getInt("age"));
+                student.setName(rs.getString("name"));
+                lis.add(student);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (conn != null && !conn.isClosed()) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
         return lis;
     }
 }
